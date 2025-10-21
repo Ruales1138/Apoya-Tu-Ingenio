bb157412c21e71e0996ea553d61f6713322d426e
JuanOcampo <juanjocampogomrz13@gmail.com>
2025-10-20 23:19:20 -0500

    Se añade un contenedor en el encabezado y se agrega una foto nueva al repositorio

--- mi-app/src/components/FormM/FormM.js
+++ mi-app/src/components/FormM/FormM.js
@@ -1,9 +1,15 @@
 import React from "react";
 import style from "./FormM.module.css";
-
+import logo_udem from "../../images/logo_udem.png";
+import avatar from "../../images/avatar.png";
 function FormM() {
     return(
-        <div>Form</div>
+        <div className={style.pageContainer}>
+            <header className={style.container}>
+                <img src={logo_udem} alt="Logo Universidad de Medellín" className={style.image} />
+                <img src={avatar} alt="Foto de perfil" className={style.avatar} />
+            </header>
+        </div>
     )
 };
 
