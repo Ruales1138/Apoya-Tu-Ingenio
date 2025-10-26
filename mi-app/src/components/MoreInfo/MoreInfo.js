f8762f62df7b80a1b633e1e4de87c5eb5eb87d45
David Ruales <hruales1138@gmail.com>
2025-10-26 15:48:18 -0500

    moreinfo

--- mi-app/src/components/MoreInfo/MoreInfo.js
+++ mi-app/src/components/MoreInfo/MoreInfo.js
@@ -2,30 +2,56 @@ import React from "react";
 import style from "./MoreInfo.module.css";
 import logo_subir from "../../images/logo_subir.png";
 import { Link } from 'react-router-dom';
+import logo_udem from "../../images/logo_udem.png";
+import avatar from "../../images/avatar.png";
 
-function MoreInfo() {
+export default function MoreInfo() {
   return (
-    <section className={style.applicationForm}>
-      <div className={style.formHeader}>
-        <img src={logo_subir} alt="logo_subir" />
-        {/* Bloque de texto (Título + descripción) */}
-        <div className={style.formHeaderText}>
-          <h2 className={style.formTitle}>Formulario de aplicación a monitoría académica</h2>
-          <p className={style.formDescription}>
-            Diligencia este formulario para postularte a las convocatorias de monitorías.<br />
-            La información será revisada por el docente o coordinador responsable.<br />
-            La IA puede sugerir opciones, pero la decisión final será académica.
-          </p>
+    <div>
+      {/* Header: logo left, avatar right */}
+      <div className={style.pageHeader}>
+        <div className={style.logoUdemContainer}>
+          <img src={logo_udem} alt="Logo Universidad de Medellín" className={style.logoUdem} />
         </div>
 
-        {/* Botones a la derecha */}
-        <div className={style.formButtonsColumn}>
-          <Link to="/form" className={style.submitBtn}>Enviar aplicación</Link>
-          <div className={style.aiSupportBox}>Con apoyo de IA</div>
+        <div className={style.avatarContainer}>
+          <img src={avatar} alt="Foto de perfil" className={style.avatar} />
         </div>
       </div>
-    </section>
+
+      <div className={style.content}>
+        {/* Volver */}
+        <section className={style.button_back}>
+          <div className={style.backBtnContainer}>
+            <Link to="/student" className={style.backBtn}>Volver a Convocatorias</Link>
+          </div>
+        </section>
+
+        {/* Application form */}
+        <section className={style.applicationForm}>
+          <div className={style.formHeader}>
+            <img src={logo_subir} alt="logo_subir" />
+
+            {/* Bloque de texto (Título + descripción) */}
+            <div className={style.formHeaderText}>
+              <h2 className={style.formTitle}>Formulario de aplicación a monitoría académica</h2>
+              <p className={style.formDescription}>
+                Diligencia este formulario para postularte a las convocatorias de monitorías.<br />
+                La información será revisada por el docente o coordinador responsable.<br />
+                La IA puede sugerir opciones, pero la decisión final será académica.
+              </p>
+            </div>
+
+            {/* Botones a la derecha */}
+            <div className={style.formButtonsColumn}>
+              <Link to="/form" className={style.submitBtn}>Enviar aplicación</Link>
+              <div className={style.aiSupportBox}>Con apoyo de IA</div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      
+    </ div>
   );
 }
-
-export default MoreInfo;
